3863266427c445fd8c175a2c4f53376d8e221c0c
cptPLANK <julien@aeosys.de>
2022-09-15 17:20:15 +0200

    37. Understanding Directives

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,9 +6,15 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ServerComponent } from './server/server.component';
 import { ServersComponent } from './servers/servers.component';
+import { AufgabeZweiComponent } from './aufgabe-zwei/aufgabe-zwei.component';
 
 @NgModule({
-  declarations: [AppComponent, ServerComponent, ServersComponent],
+  declarations: [
+    AppComponent,
+    ServerComponent,
+    ServersComponent,
+    AufgabeZweiComponent,
+  ],
   imports: [BrowserModule, AppRoutingModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
---
+++ src/app/aufgabe-zwei/aufgabe-zwei.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-aufgabe-zwei',
+  templateUrl: './aufgabe-zwei.component.html',
+})
+export class AufgabeZweiComponent {
+  username: string = '';
+}
